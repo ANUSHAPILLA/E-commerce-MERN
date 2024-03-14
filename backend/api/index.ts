5788a41b039253ba2c944a76b4f886d339301f05
anusha <sur.anusha@gmail.com>
2024-03-14 17:37:00 +0530

    build

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -19,25 +19,7 @@ mongoDB.connect(
 app.get("/", (req, res) => {
   res.send("Express app is running");
 });
-//Image storage Engine
-const storage = multer.diskStorage({
-  destination: "upload/images",
-  filename: (req, file, cb) => {
-    return cb(
-      null,
-      `${file.fieldname}_${Date.now()}_${path.extname(file.originalname)}`
-    );
-  },
-});
-const upload = multer({ storage: storage });
-//creating upload api for images
-app.use("/images", express.static("/tmp/upload/images"));
-app.post("/upload", upload.single("product"), (req, res) => {
-  res.json({
-    success: 1,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`,
-  });
-});
+
 //mongoose Product schema
 const Product = mongoose.model("Product", {
   id: {
